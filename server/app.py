22cca32b01dad944d7e3956c1c13cff5c8789f07
Sundeep <sundeep.kaler@gmail.com>
2024-10-21 23:31:16 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -52,6 +52,37 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    
+    def post(self):
+        username = request.json.get('username')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return make_response({'message': 'Invalid username'}, 401)
+    
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({'message': '204: No Content'}, 204)
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
